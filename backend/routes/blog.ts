cffbb6d25a7b76210f8b71c2293592f177743625
R0hitG0yal <rgoyal4122@gmail.com>
2024-02-28 04:10:29 +0530

    backend almost completed.

---
+++ backend/routes/blog.ts
@@ -0,0 +1,88 @@
+import { Hono } from "hono";
+import { jwt, sign, verify } from "hono/jwt";
+
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+export const blogRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
+
+blogRouter.use("/*", async (c, next) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  const jwt = c.req.header("Authorization");
+  if (!jwt) {
+    c.status(401);
+    return c.json({ error: "unauthorized" });
+  }
+  const token = jwt.split(" ")[1];
+  const payload = await verify(token, c.env.JWT_SECRET);
+  if (!payload) {
+    c.status(401);
+    return c.json({ error: "unauthorized" });
+  }
+  c.set("userId", payload.id);
+  await next();
+});
+
+blogRouter.post("/blog", async (c) => {
+  const authorId = c.get("userId");
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  const post = await prisma.post.create({
+    data: {
+      title: body.title,
+      content: body.content,
+      authorId,
+    },
+  });
+  return c.json({
+    id: post.id,
+  });
+});
+
+blogRouter.put("/blog", async (c) => {
+  const authorId = c.get("userId");
+
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  prisma.post.update({
+    where: {
+      id: body.id,
+      authorId,
+    },
+    data: {
+      title: body.title,
+      content: body.content,
+    },
+  });
+
+  return c.text("updated post");
+});
+
+blogRouter.get("/blog/:id", async (c) => {
+  const id = c.req.param("id");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  console.log(id);
+  const post = await prisma.post.findFirst({ where: { id } });
+  return c.json(post);
+});
---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import { userRouter } from "../routes/user";
+import { blogRouter } from "../routes/blog";
+
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>().basePath("/api/v1");
+
+app.route("/user", userRouter);
+app.route("/", blogRouter);
+
+export default app;
