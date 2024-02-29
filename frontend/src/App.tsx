f73641a736d6579e455f4bc52b5c7c2dc67d9730
R0hitG0yal <rgoyal4122@gmail.com>
2024-03-01 04:26:39 +0530

    Added the AddPost route.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Blog from "./pages/Blog";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
+import AddPost from "./pages/AddPost";
 const App: React.FC = () => {
   return (
     <BrowserRouter>
@@ -10,6 +11,7 @@ const App: React.FC = () => {
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/" element={<Signin />}></Route>
         <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/blog/edit" element={<AddPost />}></Route>
       </Routes>
     </BrowserRouter>
   );
